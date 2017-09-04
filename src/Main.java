d514e3812b0d4c0048ff61cbfec72feaece8709f
BohdanRadchuk <shpionuse@gmail.com>
2017-09-04 15:51:29 +0300

    где я проведу лето

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("как я проведу своё лето 2017?");
+        System.out.println("где я проведу лето?");
     }
 }
